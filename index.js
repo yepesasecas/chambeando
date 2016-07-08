8382dbd83d1f1de028049d3bf634b0c35c323e98
Andres Yepes <yepes07@gmail.com>
2016-07-08 18:49:51 -0500

    add body-parser

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/test';
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.post('/message', function (req, res){
-  console.log(req);
+  console.log(req.body);
   // MongoClient.connect(url, function(err, db) {
   //   assert.equal(null, err);
   //   console.log("Connected correctly to server.");
