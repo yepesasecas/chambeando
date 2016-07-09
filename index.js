742fe917eafc6f6ae34d9c5d2e472d8b62f07865
Andres Yepes <yepes07@gmail.com>
2016-07-08 21:35:23 -0500

    show messages beatiful in index page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '../public'));
 
 var assert = require('assert');
 var mongoUrl = process.env.MONGODB_URI ||
